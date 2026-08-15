0bc802b8a5002bcccae4c02fb09fbca8c0c1fb50
Aastha Sheoran <aasthasheoran147@gmail.com>
2026-08-15 22:46:50 +0530

    Time: 0 ms (100%), Space: 48 MB (88.22%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,100 +1,88 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-    //     int[] ans={-1,-1};
-    //     if(nums.length==0)
-    //     {
-    //         return ans;
-    //     }
+    // int[] ans=new int[2];
+    // ans[0]=firstoccurence(nums,target);
+    // ans[1]=lastoccurence(nums,target);
 
-    //     ans[0]=firstoccurence(nums,target);
-    //     ans[1]=lastoccurence(nums,target);
-
-    //     return ans;
+    // return ans;
     // }
 
-    // public int firstoccurence(int nums[],int target)
-    // {
-    //     int s=0;
-    //     int e=nums.length-1;
+    // static int firstoccurence(int[] nums,int target){
+    //     int low=0,high=nums.length-1;
     //     int ans=-1;
-    //     while(s<=e)
-    //     {
-    //         int mid=(s+e)/2;
+    //     while(low<=high){
+    //         int mid=low+(high-low)/2;
+
     //         if(nums[mid]==target){
     //             ans=mid;
-    //             e=mid-1;
-    //         }else if(target>nums[mid])
-    //         {
-    //             s=mid+1;
-    //         }else{
-    //             e=mid-1;
+    //             high=mid-1;
+    //         }
+    //         else if(nums[mid]<target){
+    //             low=mid+1;
+    //         }
+    //         else{
+    //             high=mid-1;
     //         }
     //     }
     //     return ans;
     // }
 
-    //  public int lastoccurence(int nums[],int target)
-    // {
-    //     int s=0;
-    //     int e=nums.length-1;
+    //   static int lastoccurence(int[] nums,int target){
+    //     int low=0,high=nums.length-1;
     //     int ans=-1;
-    //     while(s<=e)
-    //     {
-    //         int mid=(s+e)/2;
+    //     while(low<=high){
+    //         int mid=low+(high-low)/2;
+
     //         if(nums[mid]==target){
     //             ans=mid;
-    //             s=mid+1;
-    //         }else if(target>nums[mid])
-    //         {
-    //             s=mid+1;
-    //         }else{
-    //             e=mid-1;
+    //             low=mid+1;;
+    //         }
+    //         else if(nums[mid]<target){
+    //             low=mid+1;
+    //         }
+    //         else{
+    //             high=mid-1;
     //         }
     //     }
     //     return ans;
 
-    int[] ans=new int[2];
-    ans[0]=firstoccurence(nums,target);
-    ans[1]=lastoccurence(nums,target);
-
-    return ans;
+    int lb=lowerbound(nums,target);
+    if(lb==nums.length || nums[lb]!=target){
+        return new int[] {-1,-1};
+    }
+    else{
+        return new int[] {lb,upperbound(nums,target)-1};
     }
 
-    static int firstoccurence(int[] nums,int target){
+    }
+
+    static int lowerbound(int[] nums,int target){
         int low=0,high=nums.length-1;
-        int ans=-1;
+        int ans=nums.length;
         while(low<=high){
             int mid=low+(high-low)/2;
-
-            if(nums[mid]==target){
+            if(nums[mid]>=target){
                 ans=mid;
                 high=mid-1;
             }
-            else if(nums[mid]<target){
-                low=mid+1;
-            }
             else{
-                high=mid-1;
+                low=mid+1;
             }
         }
         return ans;
     }
 
-      static int lastoccurence(int[] nums,int target){
+    static int upperbound(int[] nums,int target){
         int low=0,high=nums.length-1;
-        int ans=-1;
+        int ans=nums.length;
         while(low<=high){
             int mid=low+(high-low)/2;
-
-            if(nums[mid]==target){
+            if(nums[mid]>target){
                 ans=mid;
-                low=mid+1;;
-            }
-            else if(nums[mid]<target){
-                low=mid+1;
+                high=mid-1;
             }
             else{
-                high=mid-1;
+                low=mid+1;
             }
         }
         return ans;
