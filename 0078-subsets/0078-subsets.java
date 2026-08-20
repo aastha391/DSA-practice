45c3f8928e3da5e43577a9014f55a32f93191b21
Aastha Sheoran <aasthasheoran147@gmail.com>
2026-08-20 15:09:01 +0530

    Time: 16 ms (0.92%), Space: 45.4 MB (7.02%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,41 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        int n=nums.length;
-        int subset=(1<<n);
-        List<List<Integer>> ans=new ArrayList<>();
-        for(int num=0;num<=subset-1;num++){
-            List<Integer> list=new ArrayList<>();
-            for(int i=0;i<n;i++){
-                if((num & (1<<i)) != 0){
-                    list.add(nums[i]);
-                }
-            }
-            ans.add(list);
-        }
+        // int n=nums.length;
+        // int subset=(1<<n);
+        // List<List<Integer>> ans=new ArrayList<>();
+        // for(int num=0;num<=subset-1;num++){
+        //     List<Integer> list=new ArrayList<>();
+        //     for(int i=0;i<n;i++){
+        //         if((num & (1<<i)) != 0){
+        //             list.add(nums[i]);
+        //         }
+        //     }
+        //     ans.add(list);
+        // }
+        // return ans;
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        solve(nums, 0, new ArrayList<>(), ans);
+
         return ans;
+    }
+
+    static void solve(int[] nums, int index,
+                      List<Integer> current,
+                      List<List<Integer>> ans) {
+        System.out.println(current);
+
+        if (index == nums.length) {
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+
+        List<Integer> take = new ArrayList<>(current);
+        take.add(nums[index]);
+
+        solve(nums, index+1, take, ans);
+
+        solve(nums, index+1, current, ans);
     }
 }
