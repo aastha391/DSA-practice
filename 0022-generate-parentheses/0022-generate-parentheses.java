41dbf78ded0c275cb19f81431021a53533fe2146
Aastha Sheoran <aasthasheoran147@gmail.com>
2026-08-21 16:20:21 +0530

    Time: 2 ms (70.1%), Space: 44.8 MB (44.5%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+
+        solve(n, 0, 0, "", ans);
+
+        return ans;
+    }
+
+    static void solve(int n, int open, int close,
+                      String curr, List<String> ans) {
+
+        if (curr.length() == 2 * n) {
+            ans.add(curr);
+            return;
+        }
+
+        if (open < n) {
+            solve(n, open + 1, close,
+                  curr + "(", ans);
+        }
+        
+        if (close < open) {
+            solve(n, open, close + 1,
+                  curr + ")", ans);
+        }
+    }
+}
